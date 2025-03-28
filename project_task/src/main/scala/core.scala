b8e9b7a881b6129f9db333b9d37dfd9607339da1
Lorenzo Campana <lorenzo.campana@edu.rptu.de>
2025-03-28 16:18:05 +0100

    some corrections in core.scala

--- project_task/src/main/scala/core.scala
+++ project_task/src/main/scala/core.scala
@@ -8,21 +8,20 @@ import chisel3.experimental.ChiselEnum
 import chisel3.util._
 import chisel3.util.experimental.loadMemoryFromFile
 
-// -----------------------------------------
-// Definition of branch target buffer
-// -----------------------------------------
+// --------------------------------------
+// | Definition of branch target buffer |
+// --------------------------------------
 
 /* Bundles within the BTB */
 class BtbEntry extends Bundle {
-  val valid = UInt(1.W)
-  val tag = UInt(29.W)
+  val valid          = UInt(1.W)
+  val tag            = UInt(29.W)
   val target_address = UInt(32.W)
-  val prediction = UInt(2.W)
-  //val prediction = StateBranchTargetBuffer.State() // 2-bit FSM (see line 30) why ? why not numbers?
+  val prediction     = UInt(2.W)
 }
 
 class BtbSet extends Bundle {
-  val ways = Vec(2, new BtbEntry())
+  val ways        = Vec(2, new BtbEntry())
   val LRU_counter = UInt(1.W) // if 0 then way 0 was most recently used, if 1 then way 1 most recenlty used
 }
 
@@ -39,38 +38,38 @@ class BranchTargetBuffer extends Module{
   import StateBranchTargetBuffer.State._
   // Inputs/Outputs
   val io = IO(new Bundle {
-    val PC  = Input(UInt(32.W))
-    val update  = Input(UInt(1.W))
-    val updatePC  = Input(UInt(32.W))
+    val PC            = Input(UInt(32.W))
+    val update        = Input(UInt(1.W))
+    val updatePC      = Input(UInt(32.W))
     val updateTarget  = Input(UInt(32.W))
     val mispredicted  = Input(UInt(1.W)) //1= miss 0= good pred
 
-    val valid = Output(UInt(1.W))
-    val target = Output(UInt(32.W))
-    val predictTaken = Output(UInt(1.W))
+    val valid         = Output(UInt(1.W))
+    val target        = Output(UInt(32.W))
+    val predictTaken  = Output(UInt(1.W))
   })
   // Structure
   val btb = RegInit(VecInit(Seq.fill(8)(0.U.asTypeOf(new BtbSet()))))
 
   // Index and Tag extraction
   val index = io.PC(4,2)
-  val tag = io.PC(31,5)
+  val tag   = io.PC(31,5)
 
   // Check if any way has a matching tag
   val way0Match = btb(index).ways(0).valid & (btb(index).ways(0).tag === tag)
   val way1Match = btb(index).ways(1).valid & (btb(index).ways(1).tag === tag)
-  val hit = way0Match | way1Match
+  val hit       = way0Match | way1Match
 
   // Select the correct way if there is a match
   val waySel = Mux((way0Match).asBool, 0.U, 1.U)
 
   // Get the predictor state
-  val predState = btb(index).ways(waySel).prediction
+  val predState      = btb(index).ways(waySel).prediction
   val predictedTaken = predState >= 1.U // StateBranchTargetBuffer.State.WeakNotTaken
 
-  //all of this happens after IF
+  // (all of this happens after IF)
 
-    //checking if target address is in btb
+    // Checking if target address is in BTB
     val currentSet = btb(index) 
 
     when(currentSet.ways(0).tag === tag){ //get the 1st entry
@@ -93,14 +92,11 @@ class BranchTargetBuffer extends Module{
     }
 
 
-  // all this happens after EX stage
+  // (all this happens after EX stage)
  
-  /* State Machine Transition Logic (following the scheme sl.6-47) */
+  // State Machine Transition Logic (following the scheme sl.6-47)
   
-  val stateBtb = RegInit(State.WeakNotTaken)
-  //starting state
-  //stateBtb := State.WeakNotTaken
-  
+  val stateBtb = RegInit(State.WeakNotTaken)  
 
     switch(stateBtb){
       is(State.StrongNotTaken){ //00
@@ -111,21 +107,21 @@ class BranchTargetBuffer extends Module{
         }
       }
       is(State.WeakNotTaken){ //01
-        when(io.mispredicted=== 1.U){
+        when(io.mispredicted === 1.U){
           stateBtb := State.WeakTaken
         }.otherwise{
           stateBtb := State.StrongNotTaken
         }
       }
       is(State.StrongTaken){ //10
-        when(io.mispredicted=== 1.U){
+        when(io.mispredicted === 1.U){
           stateBtb := State.WeakTaken
         }.otherwise{
           stateBtb := State.StrongTaken
         }
       }
       is(State.WeakTaken){ //11
-        when(io.mispredicted=== 1.U){
+        when(io.mispredicted === 1.U){
           stateBtb := State.StrongNotTaken
         }.otherwise{
           stateBtb := State.StrongTaken
@@ -144,7 +140,7 @@ class BranchTargetBuffer extends Module{
       currentSet.ways(0).valid := 1.U
       currentSet.ways(0).tag := io.updatePC(31,5)
       currentSet.ways(0).target_address := io.updateTarget
-      currentSet.LRU_counter := 1.U
+      currentSet.LRU_counter := 0.U
       //update counter
       when(((io.mispredicted & io.predictTaken) | (~io.predictTaken & io.mispredicted)) === 1.U){
         currentSet.ways(0).prediction := currentSet.ways(0).prediction - 1.U
@@ -155,7 +151,7 @@ class BranchTargetBuffer extends Module{
       currentSet.ways(1).valid := 1.U
       currentSet.ways(1).tag := io.updatePC(31,5)
       currentSet.ways(1).target_address := io.updateTarget
-      currentSet.LRU_counter := 0.U
+      currentSet.LRU_counter := 1.U
       //update counter
       when(((io.mispredicted & io.predictTaken) | (~io.predictTaken & io.mispredicted)) === 1.U){
         currentSet.ways(1).prediction := currentSet.ways(1).prediction - 1.U
@@ -166,8 +162,7 @@ class BranchTargetBuffer extends Module{
   }
 
   // Output Connections
-  io.valid := hit
-  io.target := Mux((hit).asBool, btb(index).ways(waySel).target_address, 0.U)
+  io.valid        := hit
+  io.target       := Mux((hit).asBool, btb(index).ways(waySel).target_address, 0.U)
   io.predictTaken := Mux((hit).asBool, predictedTaken, false.B)
 }
-
